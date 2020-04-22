ec00859273b991d49884475842f1ff56a21758ab
David T <dtchin2@gmail.com>
2020-04-22 01:28:22 -0400

    Initial Commit

---
+++ src/main/java/com/rta/wc/RockyTruckingWebconsoleApplication.java
@@ -0,0 +1,13 @@
+package com.rta.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockyTruckingWebconsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockyTruckingWebconsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rta/wc/RockyTruckingWebconsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rta.wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockyTruckingWebconsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
